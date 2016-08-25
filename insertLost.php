f9ded1649961102d3f84bca36bf9284574de5ef1
KennedyWaweru <kennedywaweru17@gmail.com>
2016-08-25 14:24:26 +0300

    Create fetchLost.php

---
+++ insertLost.php
@@ -0,0 +1,29 @@
+<?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require 'connect.php';
+
+
+
+
+if (isset($_POST["name"])) 
+{extract($_POST);
+	//mysql_select_db("Lost_kid");
+	$sql="INSERT INTO test_lost values('','$name','$gender','$age','$date','$description',	'$parent_name','$parent_contact')";
+	$res=@mysql_query($sql)or die(mysql_error());;
+		//
+	if($res)
+	{
+        echo "Success";
+	}
+	else
+	{
+		echo "Failed";
+	}
+}
+else{
+	//header("location:index.php");
+	echo "No data sent";
+}
+?>
